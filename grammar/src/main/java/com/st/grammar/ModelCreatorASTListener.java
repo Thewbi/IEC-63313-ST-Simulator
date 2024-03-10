eded89e3d26869f00b46ffca84c09b9b0e8af435
W.Bischoff <w.bischoff@geze.com>
2024-03-10 10:18:56 +0100

    Removed commented code

--- grammar/src/main/java/com/st/grammar/ModelCreatorASTListener.java
+++ grammar/src/main/java/com/st/grammar/ModelCreatorASTListener.java
@@ -29,11 +29,7 @@ public class ModelCreatorASTListener extends StructuredTextBaseListener {
 
     private DataType dataType = DataType.UNKNOWN;
 
-    // private Statement statement;
-
     private List<Expression> expressionList = new ArrayList<>();
-
-    // private List<Statement> statementList = new ArrayList<>();
 
     private Expression variableExpression;
 
@@ -46,10 +42,6 @@ public class ModelCreatorASTListener extends StructuredTextBaseListener {
     private boolean equalsDetected;
 
     private boolean notEqualsDetected;
-
-    // private boolean addOperatorFound;
-
-    // private boolean subtractOperatorFound;
 
     @Override
     public void enterProgram_declaration(StructuredTextParser.Program_declarationContext ctx) {
@@ -113,41 +105,13 @@ public class ModelCreatorASTListener extends StructuredTextBaseListener {
         // ctx.getStart().getText());
 
         dataType = DataType.valueOf(ctx.getStart().getText());
-        // variable.setDataType(dataType);
     }
-
-    // @Override
-    // public void exitStatement(StructuredTextParser.StatementContext ctx) {
-    // System.out.println(ctx.getClass().getSimpleName() + " " +
-    // ctx.getStart().getText());
-
-    // if (statement instanceof AssignmentStatement) {
-    // AssignmentStatement assignmentStatement = (AssignmentStatement) statement;
-
-    // // use the parse expression and copy it into the statement
-    // assignmentStatement.setExpression(expressionList.get(0));
-
-    // assignmentStatement = null;
-    // }
-
-    // // clear the expression list so it is not accidently used by the next
-    // statement
-    // expressionList.clear();
-    // }
 
     @Override
     public void exitAssignment_statement(StructuredTextParser.Assignment_statementContext ctx) {
 
         AssignmentStatement assignmentStatement = new AssignmentStatement();
-        // statementList.add(assignmentStatement);
-        // statement = assignmentStatement;
         scopeStack.peek().addStatement(assignmentStatement);
-
-        // // insert variable name
-        // System.out.println(ctx.variable());
-        // VariableContext variableContext = ctx.variable();
-        // final String varName = variableContext.getStart().getText();
-        // assignmentStatement.setVariableQualifier(varName);
 
         // the expression is added into the statement in exitStatement
         // use the parse expression and copy it into the statement
@@ -157,60 +121,6 @@ public class ModelCreatorASTListener extends StructuredTextBaseListener {
         expressionList.clear();
     }
 
-    // @Override
-    // public void
-    // exitAssignment_statement(StructuredTextParser.Assignment_statementContext
-    // ctx) {
-    // Scope topScope = scopeStack.peek();
-
-    // AssignmentStatement assignmentStatement = new AssignmentStatement();
-    // // statement = assignmentStatement;
-
-    // assignmentStatement.setVariableQualifier(variableExpression.getVariableNameValue());
-    // assignmentStatement.setExpression(assignmentExpression);
-
-    // topScope.addStatement(assignmentStatement);
-    // }
-
-    // @Override
-    // public void exitVariable(StructuredTextParser.VariableContext ctx) {
-
-    // // AssignmentStatement assignmentStatement = (AssignmentStatement) statement;
-
-    // // Expression expression = expressionList.get(0);
-    // //
-    // assignmentStatement.setVariableQualifier(expression.getVariableNameValue());
-
-    // variableExpression = expressionList.get(0);
-
-    // expressionList.clear();
-    // }
-
-    // @Override
-    // public void
-    // exitAssignment_statement(StructuredTextParser.Assignment_statementContext
-    // ctx) {
-    // System.out.println(ctx.getClass().getSimpleName() + " " +
-    // ctx.getStart().getText());
-
-    // Scope topScope = scopeStack.peek();
-
-    // AssignmentStatement assignmentStatement = new AssignmentStatement();
-    // statement = assignmentStatement;
-    // topScope.addStatement(assignmentStatement);
-
-    // // insert variable name
-    // System.out.println(ctx.variable());
-    // VariableContext variableContext = ctx.variable();
-    // final String varName = variableContext.getStart().getText();
-    // assignmentStatement.setVariableQualifier(varName);
-
-    // // the expression is added into the statement in exitStatement
-    // // use the parse expression and copy it into the statement
-    // assignmentStatement.setExpression(expressionList.get(0));
-    // expressionList.clear();
-    // }
-
     @Override
     public void enterRepeat_statement(StructuredTextParser.Repeat_statementContext ctx) {
         // System.out.println(ctx.getClass().getSimpleName() + " " +
@@ -219,8 +129,6 @@ public class ModelCreatorASTListener extends StructuredTextBaseListener {
         Scope topScope = scopeStack.peek();
 
         RepeatStatement repeatStatement = new RepeatStatement();
-        // statement = repeatStatement;
-        // topScope.addStatement(repeatStatement);
 
         // repeat statement is the new scope
         scopeStack.push(repeatStatement);
@@ -231,45 +139,16 @@ public class ModelCreatorASTListener extends StructuredTextBaseListener {
 
         RepeatStatement repeatStatement = (RepeatStatement) scopeStack.pop();
 
-        // RepeatStatement repeatStatement = (RepeatStatement) statement;
-
-        // repeatStatement.getStatements().addAll(statementList);
-        // statementList.clear();
-
         repeatStatement.setTerminationCondition(comparisonExpression);
         comparisonExpression = null;
 
         scopeStack.peek().addStatement(repeatStatement);
-
     }
-
-    // @Override
-    // public void exitExpression(StructuredTextParser.ExpressionContext ctx) {
-    // // System.out.println(ctx.getClass().getSimpleName() + " " +
-    // // ctx.getStart().getText());
-
-    // // System.out.println(expressionList);
-    // // expressionList.clear();
-
-    // // AssignmentStatement assignmentStatement = (AssignmentStatement) statement;
-
-    // // assignmentExpression = expressionList.get(0);
-
-    // // System.out.println(assignmentExpression);
-    // // assignmentStatement.setExpression(expression);
-
-    // // expressionList.clear();
-    // }
 
     @Override
     public void exitComparison(StructuredTextParser.ComparisonContext ctx) {
         // System.out.println(ctx.getClass().getSimpleName() + " " +
         // ctx.getStart().getText());
-
-        // // if the is only a single operand, no comparison node is required
-        // if (expressionList.size() <= 1) {
-        // return;
-        // }
 
         if ((!equalsDetected) && (!notEqualsDetected)) {
             return;
@@ -285,15 +164,6 @@ public class ModelCreatorASTListener extends StructuredTextBaseListener {
         comparisonExpression.getExpressionList().addAll(expressionList);
 
         expressionList.clear();
-
-        // expressionList.add(expression);
-
-        // Scope topScope = scopeStack.peek();
-        // System.out.println(topScope);
-
-        // System.out.println(statement);
-
-        // topScope.addStatement(statement);
     }
 
     @Override
@@ -361,62 +231,9 @@ public class ModelCreatorASTListener extends StructuredTextBaseListener {
         expressionList.add(arithmeticExpression);
     }
 
-    // @Override
-    // public void exitAdd_expression(StructuredTextParser.Add_expressionContext
-    // ctx) {
-
-    // // if the is only a single operand, no and node is required
-    // if ((!addOperatorFound) && (!subtractOperatorFound)) {
-    // return;
-    // }
-
-    // // build a new Add-Expression and copy all the expression parsed so far into
-    // it
-    // Expression expression = new Expression();
-    // if (addOperatorFound) {
-    // expression.setExpressionType(ExpressionType.ADD);
-    // }
-    // if (subtractOperatorFound) {
-    // expression.setExpressionType(ExpressionType.SUBTRACT);
-    // }
-    // expression.getExpressionList().addAll(expressionList);
-
-    // // remove the expressions parsed so far
-    // expressionList.clear();
-
-    // // place the new add expression into the list so it can be used by further
-    // // expressions
-    // expressionList.add(expression);
-
-    // addOperatorFound = false;
-    // subtractOperatorFound = false;
-    // }
-
-    // @Override
-    // public void exitAdd_operator(StructuredTextParser.Add_operatorContext ctx) {
-
-    // System.out.println(ctx.getStart().getText());
-
-    // String operatorAsString = ctx.getStart().getText();
-
-    // if (StringUtils.equalsIgnoreCase(operatorAsString, "+")) {
-    // addOperatorFound = true;
-    // } else if (StringUtils.equalsIgnoreCase(operatorAsString, "-")) {
-    // subtractOperatorFound = true;
-    // }
-    // }
-
     @Override
     public void visitTerminal(TerminalNode node) {
         // System.out.println("TERMINAL: " + node.getText());
-
-        // if (StringUtils.equalsIgnoreCase("UNTIL", node.getText())) {
-        // Scope scope = scopeStack.peek();
-        // if (scope instanceof RepeatStatement) {
-        // RepeatStatement repeatStatement = (RepeatStatement) scope;
-        // repeatStatement.setUntilDetected(true);
-        // }
-        // }
 
         if (StringUtils.equalsIgnoreCase("=", node.getText())) {
             equalsDetected = true;
